b5f884697ddaeaa3e92b324ab704ba2175a5b912
Mihajlo <mihajlostojanovic091999@gmail.com>
2022-12-02 08:16:51 +0100

    Update

--- src/components/NotFound/index.tsx
+++ src/components/NotFound/index.tsx
@@ -1,7 +1,7 @@
 function NotFound() {
   return (
     <div className="not-found flex w-full  flex-col  items-center text-lg  ">
-      <h1 className=" my-[50px] block text-[120px] text-[white]">404</h1>
+      <h1 className=" my-[50px] block text-[120px] dark:text-[white]">404</h1>
       <span className="my-[50px] block  text-[40px]">
         Ooops! Something wrong...
       </span>
--- src/containers/Home/index.tsx
+++ src/containers/Home/index.tsx
@@ -5,7 +5,7 @@ import Form from './Form';
 
 function Home() {
   return (
-    <div className="dark:bg-[#202c37] ">
+    <div className="min-h-[100vh] dark:bg-[#202c37]">
       <Form />
       <Cards />
     </div>
